44752d23163a7df79b5722310105e124ab2d109e
imdreamer <743295483@qq.com>
2020-10-15 23:04:56 +0800

    [yangqian] complete.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,25 @@
-export const getPoetry
+export const getPoetry = () => {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Accept: "application/json;charset=UTF-8",
+      "Content-Type": "application/json",
+      Cache: "no-cache",
+    },
+  };
+  return fetch("https://v1.jinrishici.com/all.json", requestOptions).then(
+    (response) => {
+      return response.json().then((data) => {
+        if (!response.ok) {
+          const error = response.statusText;
+          return Promise.reject(error);
+        }
+        const result = [];
+        result.push(data.origin);
+        result.push(data.author);
+        result.push(data.content);
+        return result;
+      });
+    }
+  );
+};
